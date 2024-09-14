0138eed0475c92ba44b84f73a598cc6b943aebf6
kaneru <sojusings@gmail.com>
2024-09-14 15:15:13 -0400

    removing api key

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,10 @@ import GoogleAddressAutocomplete from "./components/GooglePlacesAutocomplete.vue
 import InfoCard from "./components/InfoCard.vue";
 import SelectTime from "./components/SelectTime.vue";
 import ThemeController from "./components/ThemeController.vue";
-import InfoCard from "./components/InfoCard.vue";
 import { DayTime } from "./interfaces/DayTime";
 import { Place } from "./interfaces/Place";
 
-const apiKey = "AIzaSyCEMuCbz0M1BBy6axO42nV5PkBhvB6gvjU";
+const apiKey = "";
 const query = ref<string>("");
 const selectedPlace = ref<Place | undefined>(undefined);
 const selectedDayTime = ref<DayTime | undefined>(undefined);
