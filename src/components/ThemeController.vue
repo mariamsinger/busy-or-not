30b02f0cbdc67780c48964930eb597ed1a9ae3dc
kaneru <sojusings@gmail.com>
2024-09-14 11:39:29 -0400

    theme switcher styling update

--- src/components/ThemeController.vue
+++ src/components/ThemeController.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-    <label class="flex cursor-pointer gap-2">
+    <label class="flex cursor-pointer gap-2 items-center">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
             viewBox="0 0 24 24"
             fill="none"
+            class="opacity-70"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
@@ -15,13 +16,14 @@
         >
             <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
         </svg>
-        <input type="checkbox" value="light" class="toggle theme-controller" />
+        <input type="checkbox" value="light" class="opacity-70 toggle theme-controller" />
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
             viewBox="0 0 24 24"
             fill="none"
+            class="opacity-70"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
